33b706664a88ac5f5ad349e2ee2eb9b08fb9e8a4
hyktv <hafsa.yktv@gmail.com>
2024-01-19 22:10:07 -0800

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldsProperties_Hafsa
+{
+    public class Person
+    {
+        //private feild - encapsulates the data to prevent direct external access
+        private string _name;
+
+        public string Name
+        { 
+            get { return _name; } //getter - returns the calue of _name
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                    _name = value;
+                else
+                    throw new ArgumentException("Name cannot be null or empty");
+            } //setter - assigns a value to _name
+
+        } //Name
+
+        //readonly properties include a get accessor but no set accessor
+        public string ReadOnlyName
+        {
+            get { return _name; }
+
+        }//ReadOnlyName
+
+        //writeonly properties include a set accessor but no get accessor
+        public string WriteOnlyName
+        {
+            set { _name = value; }
+
+        }//WriteOnlyName
+
+
+    } //class
+
+
+}//namespace
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GA_FieldsProperties_Hafsa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "John Doe"; //testing setter
+            Console.WriteLine(person.Name); //testing getter
+            Console.WriteLine(person.ReadOnlyName); //testing readonly property
+        }//main
+
+    }//class
+
+
+}//namespace
